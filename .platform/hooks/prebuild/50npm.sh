51e0d2ab31c02a738331c9cdda61a562327ca082
hanaldo <jiw021538@gmail.com>
2021-07-11 21:15:32 +0900

    Fix: modify disable npm script file contents and name and create enable npm script file after build

--- .platform/hooks/prebuild/50npm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Prevent installing or rebuilding like Elastic Beanstalk tries to do by
-# default.
-#
-# Note that this *overwrites* Elastic Beanstalk's default 50npm.sh script
-# (https://gist.github.com/wearhere/de51bb799f5099cec0ed28b9d0eb3663).
